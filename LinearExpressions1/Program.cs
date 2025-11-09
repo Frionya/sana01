4dc9abee5647c694142d46dae15437e0003e3261
MaxNestercuk <ipz_nmm@student.ztu.edu.ua>
2025-11-09 17:46:58 +0200

    код готовий

--- LinearExpressions1/Program.cs
+++ LinearExpressions1/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class LinearExpressions1
+{
+    static void Main()
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        Console.Write("Введіть a: ");
+        double a = double.Parse(Console.ReadLine());
+        Console.Write("Введіть b: ");
+        double b = double.Parse(Console.ReadLine());
+        Console.Write("Введіть c: ");
+        double c = double.Parse(Console.ReadLine());
+        Console.Write("Введіть d: ");
+        double d = double.Parse(Console.ReadLine());
+        double x = (a + 2 * b - c + d) / (c * d)
+                   + (a + b) / (c - d)
+                   + Math.Pow(a, 2) / Math.Pow(b, 2);
+
+        double y = (5 * (a + b) * (c - d)) / (0.5 * c)
+                   + Math.Pow(d, 2) * (Math.Pow(a, 2) - Math.Pow(b, 2)) / (b - a);
+
+        double z = ((Math.Pow(x, 2) - 2 * x) * (Math.Pow(x, 3) - 4 * (Math.Pow(x, 4) + 1)) * (1 - b))
+                   / (5 * a + 3 * b);
+
+        double r = (0.5 * a + (3.0 / 4.0) * b - 7.0 / 5.0)
+                   / (3 * c + 1)
+                   + 1.0 / (a - c);
+        Console.WriteLine($"\nРезультати обчислень (LinearExpressions1):");
+        Console.WriteLine($"x = {x}");
+        Console.WriteLine($"y = {y}");
+        Console.WriteLine($"z = {z}");
+        Console.WriteLine($"r = {r}");
+    }
+}
